70dcadb42f870561237532d5d334402e9a0f8600
Vilas-cp <vilaspgowda1000@gmail.com>
2024-07-25 03:04:20 +0530

    pushing to vercel

--- app/page.tsx
+++ app/page.tsx
@@ -18,31 +18,31 @@ const features = [
     icon: GlobeIcon,
   },
   {
-    name: "Store your PDF Documents",
+    name: "Store your PDF Document",
     description:
       "Keep all your important PDF files security stored and easily accessible anytime,anywhere",
     icon: ZapIcon,
   },
   {
-    name: "Store your PDF Documents",
+    name: "Store your PDF Documen",
     description:
       "Keep all your important PDF files security stored and easily accessible anytime,anywhere",
     icon: BrainCogIcon,
   },
   {
-    name: "Store your PDF Documents",
+    name: "Store your PDF Docume",
     description:
       "Keep all your important PDF files security stored and easily accessible anytime,anywhere",
     icon: EyeIcon,
   },
   {
-    name: "Store your PDF Documents",
+    name: "Store your PDF Docum",
     description:
       "Keep all your important PDF files security stored and easily accessible anytime,anywhere",
     icon: ServerCogIcon,
   },
   {
-    name: "Store your PDF Documents",
+    name: "Store your PDF Docu",
     description:
       "Keep all your important PDF files security stored and easily accessible anytime,anywhere",
     icon: MonitorSmartphoneIcon,
@@ -98,7 +98,7 @@ export default function Home() {
         <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
           <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gird-cols-3 lg:gap-x-0 lg:gap-y-16">
             {features.map((feature) => (
-              <div className="relative pl-9">
+              <div className="relative pl-9" key={feature.name}>
                 <dt className="inline font-semibold text-gray-900">
                   <feature.icon
                     aria-hidden="true"
